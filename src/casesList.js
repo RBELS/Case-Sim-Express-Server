395a3be128a523d1060f321c345a7d732c6b084e
Artyom Belsky <artyombel2003@gmail.com>
2020-07-27 01:27:57 +0300

    Option to get one case info was added.

--- src/casesList.js
+++ src/casesList.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const { baseImgUrl } = require('./instances');
 
 const mongoClient = new MongoClient("mongodb://localhost:27017/", { useNewUrlParser: true, useUnifiedTopology: true });
 const casesList = express();
@@ -9,11 +10,22 @@ casesList.get('/:id?', (req, res) => {
         if (err) return console.log(err);
 
         if (!req.params.id) {
-            const casesQuery = await client.db('casesim').collection('cases').find({}).toArray();
-            const cases = casesQuery.map(({ _id, id, name, avatar, price }) => ({ key: _id, id, name, avatar, price }));
+            const cases = await client.db('casesim').collection('cases').find({}).toArray();
+            // const cases = casesQuery.map(({ _id, id, name, avatar, price }) => ({ key: _id, id, name, avatar, price }));
             res.status(200).json(cases);
         } else {
-            res.status(200).json("One case");
+            const caseid = parseInt(req.params.id);
+            const caseObj = await client.db('casesim').collection('cases').findOne({ id: caseid });
+            if (!caseObj) res.status(404).json({
+                error: "Error 404: Not Found."
+            }).end();
+
+
+
+            res.status(200).json({
+                ...caseObj,
+                items: caseObj.items.map((item => ({...item, avatar: `${baseImgUrl}/img/${caseObj.name}/${item.id}.png` })))
+            });
         }
     });
 });
---
+++ src/instances.js
@@ -0,0 +1,2 @@
+const baseImgUrl = "http://25.40.173.182:4000"
+module.exports.baseImgUrl = baseImgUrl;
