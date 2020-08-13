f8055e3cc4e2fdd684dc62f1cb0b87a43955b5b4
Artyom Belsky <artyombel2003@gmail.com>
2020-08-14 00:21:10 +0300

    Little changes before grate refactor.

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -1,15 +1,9 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
-const CryptoJS = require('crypto-js');
-const { secretKey } = require('./secret');
-// const amlogged = require('./amlogged/amlogged');
 const login = require('./login/login');
 const register = require('./register/register');
 
-const mongoClient = new MongoClient("mongodb://localhost:27017/", { useNewUrlParser: true, useUnifiedTopology: true });
 const auth = express();
 
-// auth.use('/amlogged/', amlogged);
 auth.use('/login/', login);
 auth.use('/register/', register);
 
